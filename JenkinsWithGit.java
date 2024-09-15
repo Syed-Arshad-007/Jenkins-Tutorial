9ead99c5a85bd3b6398e95310f1cfc908295f8ff
Syed-Arshad-007 <arshadboyaka@gmail.com>
2024-09-15 12:03:09 +0530

    Add files via upload

---
+++ JenkinsWithGit.java
@@ -0,0 +1,9 @@
+public class JenkinsWithGit {
+
+	public static void main(String[] args) {
+    System.out.println("Jenkins Tutorial with Git");
+	System.out.println("Build when commit is happening");
+    
+	}
+
+}
