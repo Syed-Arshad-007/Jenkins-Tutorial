c5ed13eee1b8c4cab7eda49d05382fd1e6ec5ae9
Syed-Arshad-007 <arshadboyaka@gmail.com>
2024-09-15 13:16:16 +0530

    Update JenkinsWithGit.java

--- JenkinsWithGit.java
+++ JenkinsWithGit.java
@@ -2,7 +2,7 @@ public class JenkinsWithGit {
 
 	public static void main(String[] args) {
     System.out.println("Jenkins Tutorial with Git");
-	System.out.println("Build when commit is happening");
+	System.out.println("Build when commit is happening updated version");
     
 	}
 
